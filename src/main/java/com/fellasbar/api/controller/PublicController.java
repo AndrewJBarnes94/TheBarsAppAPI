145d382f2e5e2599c58b3fad249c5f27599c84dd
AJ Barnes <ajbarnes@AJs-Mac-mini.local>
2026-03-18 12:58:35 -0400

    try to make it more themed

--- src/main/java/com/fellasbar/api/controller/PublicController.java
+++ src/main/java/com/fellasbar/api/controller/PublicController.java
@@ -1,13 +1,15 @@
 package com.fellasbar.api.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PublicController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("bodyClass", "coastal");
         return "home";
     }
 
@@ -22,17 +24,20 @@ public class PublicController {
     }
 
     @GetMapping("/vlogs")
-    public String vlogs() {
+    public String vlogs(Model model) {
+        model.addAttribute("bodyClass", "coastal");
         return "vlogs";
     }
 
     @GetMapping("/blog")
-    public String blog() {
+    public String blog(Model model) {
+        model.addAttribute("bodyClass", "coastal");
         return "blog";
     }
 
     @GetMapping("/store")
-    public String store() {
+    public String store(Model model) {
+        model.addAttribute("bodyClass", "coastal");
         return "store";
     }
 }
