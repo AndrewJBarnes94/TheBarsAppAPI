b8619d527c36acf9266018e99b39befe541a3bec
AJ Barnes <ajbarnes@AJs-Mac-mini.local>
2026-05-29 08:27:45 -0400

    send reset password link failed. fixed

--- src/main/java/com/fellasbar/api/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/fellasbar/api/repository/PasswordResetTokenRepository.java
@@ -2,10 +2,13 @@ package com.fellasbar.api.repository;
 
 import com.fellasbar.api.model.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
+
+    @Transactional
     void deleteByEmail(String email);
 }
